98ccc00fec5cf018b6439afe7120f52f75e0c6b8
gurra99 <30boman@gmail.com>
2024-12-28 19:01:40 +0100

    Added a query provider wrapper around navigation.

--- App.tsx
+++ App.tsx
@@ -1,19 +1,22 @@
 import { StyleSheet, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import HomeScreen from "./screens/HomeScreen";
 
+const client = new QueryClient();
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <QueryClientProvider client={client}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </QueryClientProvider>
     </View>
   );
 }
