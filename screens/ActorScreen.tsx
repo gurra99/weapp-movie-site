0d97f3ee5538a29c96c345b3c9e718e41549530e
gurra99 <30boman@gmail.com>
2024-12-28 21:32:24 +0100

    Added a query method to fetch actors.

--- screens/ActorScreen.tsx
+++ screens/ActorScreen.tsx
@@ -1,6 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
 import { StyleSheet, View } from "react-native";
+import { fetchActorsByMovieId } from "../api/actors";
+import { ScreenProps } from "../App";
 
-export default function ActorsScreen() {
+export default function ActorsScreen({
+  navigation,
+  route,
+}: ScreenProps<"Actors">) {
+  const { id, title } = route.params;
+
+  const {
+    data: actors,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["actors", id],
+    queryFn: () => fetchActorsByMovieId(id || 1),
+  });
+
+  console.log(actors);
+
   return <View></View>;
 }
 
