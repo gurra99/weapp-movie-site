48c53be8122829ed62728d55a56cb6011d31ba92
gurra99 <30boman@gmail.com>
2024-12-28 20:10:09 +0100

    Added useInfiniteQuery to fetch data.

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,6 +1,19 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { StyleSheet, View, Text } from "react-native";
+import { ITopRatedMovies } from "../models/movie";
+import { fetchTopMovies } from "../api/movies";
 
 export default function HomeScreen() {
+  const { data, isLoading, error, isFetching, hasNextPage, fetchNextPage } =
+    useInfiniteQuery<ITopRatedMovies>({
+      queryKey: ["movies"],
+      queryFn: fetchTopMovies,
+      initialPageParam: 1,
+      getNextPageParam: (lastPage: ITopRatedMovies) => {},
+    });
+
+  console.log(data);
+
   return (
     <View style={styles.container}>
       <Text>Test</Text>
